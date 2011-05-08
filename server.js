d0f1781e645fc920778f6580e93afb2748e01163
honjo2 <honjo2@gmail.com>
2011-05-08 18:03:21 +0900

    commit

--- server.js
+++ server.js
@@ -3,8 +3,43 @@ var http = require('http'),
  
 server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>socket.ioサンプル</h1>');
-    res.end();
+//    res.write('<h1>socket.ioサンプル</h1>');
+//    res.end();
+
+    res.write('<!DOCTYPE html>');
+    res.write('<html lang="ja">');
+    res.write('<meta charset="utf-8">');
+    res.write('<head>');
+    res.write('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>');
+    res.write('<script type="text/javascript" src="http://cdn.socket.io/stable/socket.io.js"></script>');
+    res.write('<script>');
+    res.write('$(function() {');
+    res.write('var socket = new io.Socket("socketiosample.duostack.net", { port: 80 });');
+    res.write('socket.connect();');
+    res.write('socket.on("message", function(message) {');
+    res.write('var m = JSON.parse(message).message;');
+    res.write('if (m) {');
+    res.write('$("#div1").append(m + "<br>");');
+    res.write('}');
+    res.write('});');
+    res.write('socket.on("disconnect", function(message) {');
+    res.write('$("#div1").append("切断されました");');
+    res.write('});');
+    res.write('$("#form1").submit(function(e) {');
+    res.write('socket.send(JSON.stringify({ message: $("#text1").val() }));');
+    res.write('return false;');
+    res.write('});');
+    res.write('});');
+    res.write('</script>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<div id="div1"></div>');
+    res.write('<form id="form1">');
+    res.write('<input id="text1" type="text"/><input type="submit"/>');
+    res.write('</form>');
+    res.write('</body>');
+    res.end('</html>');
+    
 });
 server.listen(8124);
  
