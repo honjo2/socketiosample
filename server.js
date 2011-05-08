a31445082192c8fe4afb52344d4c12a81df5d4ad
honjo2 <honjo2@gmail.com>
2011-05-08 17:27:03 +0900

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require('http'),
     io = require('socket.io'),
  
-server = http.createServer();
+server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>socket.ioサンプル</h1>');
+    res.end();
+});
 server.listen(8124);
  
 var socket = io.listen(server);
